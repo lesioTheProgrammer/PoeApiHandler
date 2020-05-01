216d202e4052dac24795a45226c29c5e2eaccd0b
Lesio Programista <lama7410@gmail.com>
2020-05-01 18:26:07 +0200

    #LB ajax async call passing results

--- PoeApiHandler/Scripts/PoeApiScripts/JSScriptHandler.js
+++ PoeApiHandler/Scripts/PoeApiScripts/JSScriptHandler.js
@@ -2,15 +2,29 @@
 
 
 
+
+let idOfnextItems;
 $(document).ready(function () {
-    document.getElementById('initialScript').onclick = function () {
-        executescript();
-    }
+
+    document.getElementById("initialScript").addEventListener("click", executescript, false);
+
+    document.getElementById("poeitems").addEventListener("click", function () {
+        getPoeItemsId().done(function (result){
+            idOfnextItems = result.next_change_id;
+            console.log("all good now" + idOfnextItems);
+        }).fail(function () {
+            console.log("An error occured sometihng is rip")
+        })
+    }, false);
 
 
-    document.getElementById('poeitems').onclick = function () {
-        getPoeItemsId();
-    }
+    document.getElementById("nextPoeItems").addEventListener("click", function () {
+        debugger;
+        xd(idOfnextItems);
+    }, false);
+  
+
+
 });
 
 
--- PoeApiHandler/Scripts/PoeApiScripts/MainPage.js
+++ PoeApiHandler/Scripts/PoeApiScripts/MainPage.js
@@ -2,34 +2,69 @@
 
 
 
-
+let nextItemsId = 0;
 
 function getPoeItemsId() {
-    $.ajax({
+   return  $.ajax({
         url: "https://api.pathofexile.com/public-stash-tabs",
         type: 'GET',
         dataType: 'json',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded' // cross site allow
         },
-        
         success: function (element) {
-            debugger;
-            var yourTableHTML = "";
+            nextItemsId = element.next_change_id; // string
             let arrayOfStashes = element.stashes;
-
+            console.log(nextItemsId + " id one");
             $.each(arrayOfStashes, function (index, item) {
                 if (item.accountName != null) {
                 $("#tablefriendsname").append("<tr><td>" + item.accountName + "</td></tr>");
                 }
             });
+            return nextItemsId; // return next id 
         },
         error: function (xhr) {
+            console.log("Theres an error")
         }
-    });
+   });
+
+};
+
+
+function xd(nextItemsId) {
+    debugger;
+    let xdd = nextItemsId;
 }
 
+//function getNextPoeItems(nextItemsId) {
+//    $.ajax({
+//        url: "https://api.pathofexile.com/public-stash-tabs",
+//        type: 'GET',
+//        dataType: 'json',
+//        headers: {
+//            'Content-Type': 'application/x-www-form-urlencoded' // cross site allow
+//        },
+//        data: {
+//            // tutaj updejt id co klik
+//            id = nextItemsId
+//        },
 
+//        success: function (element) {
+//            debugger;
+//            nextItemsId = element.next_change_id;
+//            let arrayOfStashes = element.stashes;
+//            console.log(nextItemsId + " id second");
+
+//            $.each(arrayOfStashes, function (index, item) {
+//                if (item.accountName != null) {
+//                    $("#tablefriendsname").append("<tr><td>" + item.accountName + "</td></tr>");
+//                }
+//            });
+//        },
+//        error: function (xhr) {
+//        }
+//    });
+//}
 
 
 
