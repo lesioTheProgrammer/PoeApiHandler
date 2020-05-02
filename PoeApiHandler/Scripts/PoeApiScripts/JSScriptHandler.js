969b09e4eda6d72b65ab1d2bbce65525dacd5407
Lesio Programista <lama7410@gmail.com>
2020-05-02 12:12:47 +0200

    #LB good getrequest

--- PoeApiHandler/Scripts/PoeApiScripts/JSScriptHandler.js
+++ PoeApiHandler/Scripts/PoeApiScripts/JSScriptHandler.js
@@ -4,29 +4,42 @@
 
 
 let idOfnextItems;
+let nextPoeItemsButton = false;
+
+
+
 $(document).ready(function () {
 
     document.getElementById("initialScript").addEventListener("click", executescript, false);
 
-    document.getElementById("poeitems").addEventListener("click", function () {
+    let buttonGetPoeList = document.getElementById("poeitems");
+    let buttonGetNextItems = document.getElementById("nextPoeItems");
+
+    buttonGetNextItems.disabled = true;
+
+    buttonGetPoeList.addEventListener("click", function () {
         getPoeItemsId().done(function (result){
             idOfnextItems = result.next_change_id;
+            // buttons
+            nextPoeItemsButton = true;
+            buttonGetNextItems.disabled = false;
+            buttonGetPoeList.disabled = true;
+            // buttons end
         }).fail(function () {
             console.log("An error occured sometihng is wrong")
         })
     }, false);
 
 
-    document.getElementById("nextPoeItems").addEventListener("click", function () {
-        getPoeItemsId(idOfnextItems).done(function (result) {
-            idOfnextItems = result.next_change_id;
-        }).fail(function () {
-            console.log("An error occured sometihng is wrong")
-        })
-    }, false);
-  
-
-
+    buttonGetNextItems.addEventListener("click", function () {
+        if (nextPoeItemsButton) {
+            getPoeItemsId(idOfnextItems).done(function (result) {
+                idOfnextItems = result.next_change_id;
+            }).fail(function () {
+                console.log("An error occured sometihng is wrong")
+            })
+        }}, false);
+    
 });
 
 
--- PoeApiHandler/Scripts/PoeApiScripts/MainPage.js
+++ PoeApiHandler/Scripts/PoeApiScripts/MainPage.js
@@ -1,8 +1,14 @@
 ﻿'use strict'
 
 
+//If the number of stashes returned is zero, you get back the same change ID you passed
+//    in (a hint to keep trying until the endpoint has some tabs for you).
+
+
 
 let nextItemsId = 0;
+
+
 
 function getPoeItemsId(idOfnextItems) {
    return  $.ajax({
@@ -17,7 +23,7 @@ function getPoeItemsId(idOfnextItems) {
        },
        
        success: function (element) {
-           $('tablefriendsname > tbody').empty();
+           $("#tablefriendsname tr").remove(); // swap to detach() when reusing the previous data
             nextItemsId = element.next_change_id; // string
             let arrayOfStashes = element.stashes;
             $.each(arrayOfStashes, function (index, item) {
